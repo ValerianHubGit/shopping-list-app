c5db4a8ca854963d022a35d8714fcee8ed69e421
ValerianHubGit <VKurowski@gmx.de>
2026-03-02 15:51:17 +0100

    add input field, state management and list rendering

--- frontend/App.js
+++ frontend/App.js
@@ -1,14 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput,Button, FlatList} from 'react-native';
+import { useState } from 'react'
 
 export default function App() {
+  const [artikel, setArtikel] = useState(""); 
+  const [liste, setListe] = useState([]);
+  const artikelHinzufuegen = () => {
+    if (artikel.trim() === "") return;
+    setListe([...liste, artikel]);
+    setArtikel("");
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>🛒 Meine Einkaufsliste</Text>
-      <StatusBar style="auto" />
+    <Text style={styles.title}>🛒 Meine Einkaufsliste</Text>
+    <TextInput style={styles.normaltext} placeholder="Bitte Artikel eingeben" value={artikel} onChangeText={setArtikel}/>
+    <StatusBar style="auto" />
+    <Button title="Hinzufügen" onPress={artikelHinzufuegen}/>
+    {/* Einkaufsliste: rendert jeden Artikel als Texteintrag */}
+    <FlatList data={liste}                                {/*  das Array das durchlaufen wird */}
+      keyExtractor={(item, index) => index.toString()}    {/*  eindeutiger Schlüssel pro Eintrag */}
+      renderItem={ ({ item }) => (<Text>{item}</Text>) }  {/*was für jeden Eintrag angezeigt wird */}
+    />
     </View>
   );
 }
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -21,4 +42,5 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
   },
+  normaltext:{fontSize:16}
 });
